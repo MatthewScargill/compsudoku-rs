fbbb88f004d6c8b6d793a166f055dd7b0941a94f
MatthewScargill <matthewscargilluni@gmail.com>
2025-10-29 21:01:11 +0000

    hidden single finder + working move aggregator

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,32 @@ use solver::*;
 mod board; 
 use board::*;
 
-// easy test setup with naked single
-const TEST: &str = "800000000003600000070090200050007000000045702000100030001000068008500010090000400";
+// no naked or hidden singles 
+const TEST: &str = "800000000003600000070090200050007000000045700000100030001000068008500010090000400";
 
-// test setup with naked singles 
+// naked and hidden singles 
 const TEST2: &str = "530070000600195000098000060800060003400803001700020006060000280000419005000080079";
 
 fn main() {
     let mut board = Board::new();
     //board.print();
-    board.setup(TEST2);
+    board.setup(TEST);
     board.evaluate();
     board.print();
-    let moves = solver::find_moves(&board);
+
+    let mut i = 0;
+
+    while i < 0 {
+        let moves = solver::find_moves(&board);
     
-    for mv in &moves {
-        println!("{:?}", mv);
+        for mv in &moves {
+            println!("{:?}", mv);
+        }
+
+        solver::apply_moves(&mut board, &moves);
+
+        board.print();
+        i += 1
     }
-
-    println!("{:?}", &board.grid[7][7].candidates);
-    solver::apply_moves(&mut board, &moves);
-
-    board.print()
 
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,8 +1,9 @@
 use crate::board::*;
+use std::collections::HashSet;
 
 // move type
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Move {
     NakedSingle { row: usize, col: usize, value: u8 }, // only have one candidate possible 
     HiddenSingle { row: usize, col: usize, value: u8 } // have unique candidate in row/column/subgrid 
@@ -39,11 +40,16 @@ pub fn find_moves(board: &Board) -> Vec<Move> {
     let mut hiddensingles = find_hidden_singles(board);
 
     // add all move types to move vector
-    moves = std::mem::take(&mut nakedsingles);
-    //moves = std::mem::take(&mut hiddensingles);
+    moves.extend(nakedsingles);
+    moves.extend(hiddensingles);
 
-    // return vector of moves
-    moves
+    // filter for unique moves
+    let unique_moves: Vec<Move> = HashSet::<_>::from_iter(moves).into_iter().collect();
+    // this was and forever will be a stack overflow job
+
+
+    // return vector of unique moves
+    unique_moves
 }
 
 pub fn apply_moves(board: &mut Board, moves: &Vec<Move>) {
@@ -74,6 +80,86 @@ pub fn find_naked_singles(board: &Board) -> Vec<Move> {
 }
 
 // finding cells which have a unique candidate in their row/column/subgrid
-pub fn find_hidden_singles(board: &Board) {
+// i personally hate this function it made me have to figure out how to remove duplicates and it can't be very 
+// fast but oh well, to be fixed soon (if it isn't already broken)
+pub fn find_hidden_singles(board: &Board) -> Vec<Move> {
 
+    let mut moves = Vec::new();
+
+    for row in 0..9 {
+        for digit in 1..10 {
+            let mut count = 0;
+            let mut col_pos = 0;
+
+            for col in 0..9 {
+                let cell = &board.grid[row][col];
+                if cell.value == 0 && cell.candidates[(digit - 1) as usize] {
+                    count += 1;
+                    col_pos = col;
+                }
+            }
+
+            if count == 1 {
+                moves.push(Move::HiddenSingle {
+                    row,
+                    col: col_pos,
+                    value: digit,
+                });
+            }
+        }
+    }
+    
+    for col in 0..9 {
+        for digit in 1..10 {
+            let mut count = 0;
+            let mut row_pos = 0;
+
+            for row in 0..9 {
+                let cell = &board.grid[row][col];
+                if cell.value == 0 && cell.candidates[(digit - 1) as usize] {
+                    count += 1;
+                    row_pos = row;
+                }
+            }
+
+            if count == 1 {
+                moves.push(Move::HiddenSingle {
+                    row: row_pos,
+                    col,
+                    value: digit,
+                });
+            }
+        }
+    }
+
+    for box_row in 0..3 {
+        for box_col in 0..3 {
+            for digit in 1..10 {
+                let mut count = 0;
+                let mut target = (0, 0);
+
+                for r in 0..3 {
+                    for c in 0..3 {
+                        let row = box_row * 3 + r;
+                        let col = box_col * 3 + c;
+                        let cell = &board.grid[row][col];
+
+                        if cell.value == 0 && cell.candidates[(digit - 1) as usize] {
+                            count += 1;
+                            target = (row, col);
+                        }
+                    }
+                }
+
+                if count == 1 {
+                    moves.push(Move::HiddenSingle {
+                        row: target.0,
+                        col: target.1,
+                        value: digit,
+                    });
+                }
+            }
+        }
+    }
+    moves // this produces duplicates in the moves but oh well still works for the solver for now
 }
